9bdcfab494cefe5514a28ba3fee8951574607ba1
Saumil Patel <saumil@promactinfo.com>
2022-05-11 18:44:32 +0530

    Removed unwanted code

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -10,23 +10,13 @@ import { TeamsListService } from '../teams-list.service';
 export class MainComponentComponent implements OnInit {
 
   sectionName: string | null = null;
-  teamsList !: any;
+
   selectedRow !: any;
 
   constructor(private route:ActivatedRoute, private list : TeamsListService) { }
 
   ngOnInit(): void {
     this.sectionName = this.route.snapshot.paramMap.get('section');
-    this.selectedRow = this.list.listArray.find(x => x.title == this.sectionName);
-    this.getList();
-  }
-
-  getList()
-  {
-    this.list.getTeamsList()
-    .subscribe(res=>{
-      this.teamsList = res;
-    })
-  }
+    this.selectedRow = this.list.listArray.find(x => x.title == this.sectionName);}
   
 }
